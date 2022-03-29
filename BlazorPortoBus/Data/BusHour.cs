d5da446251e7735f995ac1b7607638cbd352eae3
Rodrigo DINIZ <rdiniz@icrc.org>
2022-03-29 14:35:14 +0200

    v1

---
+++ BlazorPortoBus/Data/BusHour.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorPortoBus.Data
+{
+    public class BusHour
+    {
+        public string BusName { get; set; }
+        public string NextHour { get; set; }
+        public string WaitTime { get; set; }
+    }
+}
---
+++ BlazorPortoBus/Data/BusHourService.cs
@@ -0,0 +1,21 @@
+﻿using System.Net;
+
+namespace BlazorPortoBus.Data
+{
+    public class BusHourService
+    {       
+       
+
+        public async Task<List<BusHour>> GetHours(string code)
+        {
+            var handler = new HttpClientHandler
+            {
+                DefaultProxyCredentials = CredentialCache.DefaultCredentials
+            };
+
+            var client = new HttpClient(handler);
+            return await client
+                .GetFromJsonAsync<List<BusHour>>($"https://bushourapi.herokuapp.com/api/getTime?BusStopCode={code}");
+        }
+    }
+}
